f76e41b048f357c860f285edc1958da89312071e
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-11-14 12:59:36 +0700

    commit tree b293cdaca029e855cd8a10e743a61f79608c28b8

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 
-// Write your code here
+// ## Instructions
+
+// 1. Create a variable called `multiply` set to an equation that will multiply the
+//    variables `num1` and `num2`; the result of the multiplication should be `62`.
+
+var num1, num2, multiply
+    num1=1
+    num2=62
+    multiply = num1*num2
+
+console.log(multiply)
+// 2. Create a variable called `random` that will generate a random integer greater
+//    than 0.
+// 3. Create a variable called `mod` set to an equation that will calculate the
+//    remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+// 4. Create a variable called `max` that finds the highest number in a set; the
+//    value returned should be `20`.
+
+
+
+
+
+
+
+
+
+//SOLUTION
+
+// // Write your code here
+// //Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+// var num1,num2, multiply 
+// num1 = 1
+// num2 = 62
+// multiply = num1 * num2
+
+// console.log(multiply)
+
+// //Create a variable called random that will generate a random integer greater than 0.
+// var random = Math.floor(Math.random() * 10)
+// console.log(random)
+
+
+// //Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+// var mod, num3, num4 
+// num3 = 16
+// num4 = 6
+// mod = num3 % num4
+// console.log(mod)
+
+// //Create a variable called max that finds the highest number in a set; the value returned should be 20.
+// var max = Math.max(1, 2, 20, 4, 5);
